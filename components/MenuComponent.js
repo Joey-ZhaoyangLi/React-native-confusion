c2d6c44259377d7462ea827c420ed7997dc3e2e6
zhaoyang <zla143@sfu.ca>
2018-09-02 01:56:51 -0700

    navigation 1

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
 function Menu(props) {
 
